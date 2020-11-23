a6373c4013ad7599834631b7cf22c71978209a7f
mezzz <mezmai13@gmail.com>
2020-11-23 14:53:12 +0300

    Add test_login_form_is_element_present

--- pages/test_main_page.py
+++ pages/test_main_page.py
@@ -1,7 +1,7 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
-
-
+'''
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
@@ -13,3 +13,9 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+'''
+def test_login_form_is_element_present(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
