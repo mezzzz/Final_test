f2b5dbf1712f220a0336503d1924c8128277c959
mezzz <mezmai13@gmail.com>
2020-11-27 20:06:55 +0300

    parametrize is ready

--- pages/test_product_page.py
+++ pages/test_product_page.py
@@ -2,11 +2,13 @@ from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 import time
+import pytest
 
-
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
+                                  pytest.param(7, marks=pytest.mark.xfail),
+                                  8, 9])
+def test_guest_can_add_product_to_basket(browser,link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
     page = ProductPage(browser, link)
     page.open()
     page.can_add_product_to_basket()
-    time.sleep(5)
